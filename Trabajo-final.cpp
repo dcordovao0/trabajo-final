2e92bfb413602da64d35384ca5e4d8342707f2c6
Danny Josué Córdova Orellana <dcordovao@estud.usfq.edu.ec>
2023-04-23 15:14:35 -0500

    Creación del archivo para el proyecto

---
+++ Trabajo-final.cpp
@@ -0,0 +1,15 @@
+/*
+Trabajo final de Programación en C++ I
+Profesor: Felipe Grijalva
+Nombres: Danny Córdova/Brandom Mosquera
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
