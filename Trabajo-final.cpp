b8d814fea54964374e76dff65a3a1f033989302d
MariaCristina06 <maricrispro2004@gmail.com>
2023-05-07 07:12:31 -0500

    lectura

--- Trabajo-final.cpp
+++ Trabajo-final.cpp
@@ -5,11 +5,46 @@ Nombres: Danny Córdova/Brandom Mosquera/Cristina Proaño/ Marco Calderon
 */
 
 #include <iostream>
+#include <sstream>
 #include <vector>
-
+#include <cstring>
 using namespace std;
 
-int main()
-{
+// Lee n cadenas tipo polinomio y  quita los espacios
+ 
+struct tPolinomio {
+    double grado;
+    vector<double> coef;
+};
+	tPolinomio polinomio;
+    string cadena;
+    string cadena_valida;
+    int n; // Numero de polinomios introducidos 
+    int num_pol; // Numero de repeticiones para ingresar el polinomio
+    
+int main() {
+       
+    cout << "Ingrese la cantidad de polinomios: ";
+    cin >> num_pol;
+    cin.ignore(); // Ignoramos el salto de línea que queda en el buffer
+    
+    for (int j = 0; j < num_pol; j++) {
+        cout << "Ingrese el polinomio " << j + 1 << ": ";
+        getline(cin, cadena);
+        
+    const char* ptr = cadena.c_str();
+    for (int i = 0; i < cadena.length(); i++) {
+        if (*ptr != ' ') { // Si el caracter actual no es un espacio, lo copiamos a la nueva cadena
+            cadena_valida += *ptr;
+        }
+        ptr++; // Avanzamos al siguiente caracter
+        
+    }
+    cout << "El polinomio sin espacios: " << cadena_valida << endl;
+    cadena_valida= "";
+	
+	}
+
+	
     return 0;
 }
